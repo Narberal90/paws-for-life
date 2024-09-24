81d221c93b1f339f35d767e45d6800ebbae9b869
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-09-24 18:00:47 +0300

    added SECRET_KEY to .env file

--- paws_for_life/settings.py
+++ paws_for_life/settings.py
@@ -29,7 +29,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)*&%wu3ih&-nr3+fv!vij_3bv&v0ppx13pa8$1cm0fnowtjg%$"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
